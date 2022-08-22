2ad5de1350eaa1a5f6d7d39ff5236646af815e3d
khyojun <38220795+khyojun@users.noreply.github.com>
2022-08-23 01:02:13 +0900

    게시판 등록

--- src/test/java/com/khyojun/admin/springboot/web/IndexControllerTest.java
+++ src/test/java/com/khyojun/admin/springboot/web/IndexControllerTest.java
@@ -1,2 +1,29 @@
-package com.khyojun.admin.springboot.web;public class IndexControllerTest {
+package com.khyojun.admin.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.swing.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @Test
+    public void 게시판_등록(){
+    //when
+        String body=this.restTemplate.getForObject("/", String.class);
+
+        //then
+        Assertions.assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+    }
 }
